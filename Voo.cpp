115b42090ec2aedecf8d22475e0d99d600061cb1
tommygomez25 <maysan54321@gmail.com>
2021-11-18 23:33:19 +0000

    Added voo (for testing too)

---
+++ Voo.cpp
@@ -0,0 +1,5 @@
+//
+// Created by maysa on 18/11/2021.
+//
+
+#include "Voo.h"
---
+++ Voo.h
@@ -0,0 +1,14 @@
+//
+// Created by maysa on 18/11/2021.
+//
+
+#ifndef CLIONPROJECTS_VOO_H
+#define CLIONPROJECTS_VOO_H
+
+
+class Voo {
+
+};
+
+
+#endif //CLIONPROJECTS_VOO_H
