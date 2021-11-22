e6c3383acb404208768dfe98319c5a0681bd13f9
up202004735 <up202004735@up.pt>
2021-11-22 20:09:39 +0000

    ué

---
+++ cmake-build-debug/CMakeFiles/1_PROJECT_AED.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "1_PROJECT_AED.exe"
+  "1_PROJECT_AED.exe.manifest"
+  "1_PROJECT_AED.pdb"
+  "CMakeFiles/1_PROJECT_AED.dir/main.cpp.obj"
+  "lib1_PROJECT_AED.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1_PROJECT_AED.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
